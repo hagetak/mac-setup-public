4aa87e3a354dacaec0f0e214def6b8a880812727
kento matsushima <nyanpucchi1028@gmaill.com>
2022-03-24 19:00:12 +0900

    initial commit

---
+++ bin/provisioning.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh
+
+echo "############### You must prepare github ssh key####################"
+echo "Enter your github ssh key absolute path: "
+read private_key
+echo "Entered: $private_key"
+echo "###################################################################"
+
+cat << EOS > $HOME/.ssh/config
+Host github.com
+  HostName github.com
+  User git
+  Port 22
+  IdentityFile $private_key
+EOS
+ssh -T github.com
+
+# create src dir
+mkdir $HOME/src
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew update
+
+brew install git ghq
+
+# 初回の ghq getp のために root 変更のみ対応
+cat << EOS > $HOME/.gitconfig
+[ghq]
+  root = $HOME/src
+EOS
+
+# private repository
+ghq get -p hagetak/mac-setup
+sh $HOME/src/hagetak/mac-setup/bin/provisioning.sh
